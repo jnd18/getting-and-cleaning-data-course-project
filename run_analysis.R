3a26eaaaafdf7c42ccb99c7623f7a24bdacd3fb2
Jonathan Dorsey <jnd18@case.edu>
2018-05-15 23:37:55 -0400

    remove extraneous comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,3 @@
-# The code should have a file run_analysis.R in the main directory that can be run
-# as long as the Samsung data is in your working directory. The output should be the 
-# tidy data set you submitted for part 1. You should include a README.md in the repo 
-# describing how the script works and the code book describing the variables.
-# 
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement.
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names.
-# From the data set in step 4, creates a second, independent tidy data set with the average 
-# of each variable for each activity and each subject.
-
 # Assumes that the working directory is the UCI HAR Dataset
 # Load the tidyverse packages
 library(tidyverse)
